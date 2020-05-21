f13fe514d4eaf0b7cf9aa4358f8b112efcdb1c63
MrMushu <aaokura@gmail.com>
2020-05-20 18:19:45 -0700

    a

--- src/pages/home.js
+++ src/pages/home.js
@@ -22,64 +22,59 @@ import accent from "../images/accent.png";
 import News from "../components/news/news";
 
 const Home = () => (
-  <div>
-    <div className="Hero">
-      <div className="HeroHeader">
-        <Carousel />
-      </div>
-
-      <div className="OurFood">
-        <div className="OurFoodHeader">
-          <img
-            style={{ transform: [{ rotateX: "180deg" }] }}
-            src={accent}
-          ></img>
-          <h1>OUR FOOD</h1>
-          <img src={accent}></img>
-        </div>
-
-        <div className="OurFoodCategories">
-          <div className="DescriptionSection">
-            <img src={burrito}></img>
-            <h2>ENTREES</h2>
-          </div>
-          <div className="DescriptionSection">
-            <img src={tacos}></img>
-            <h2>MEALS</h2>
-          </div>
-          <div className="DescriptionSection">
-            <img src={white}></img>
-            <h2>SIDES</h2>
-          </div>
-        </div>
-
-        <Link to="/menu">VIEW MENU</Link>
-      </div>
-      <div className="Eclub">
-        <h1>JOIN THE ROAST</h1>
-        <div className="EclubForm">
-          <input type="text" name="email" value="email *" />
-          <Link to="/eclub">SIGN UP</Link>
-          <img src={brush}></img>
-        </div>
-      </div>
-
-      <div className="WhatsHappening">
-        <h1>WHAT'S HAPPENING</h1>
-        <div className="WhatsHappeningNews">
-          <News pic={doctors} />
-        </div>
-      </div>
-
-      <div className="Location">
-        <h1>FIND A LOCATION</h1>
-        <div className="EclubForm">
-          <input type="text" name="email" value="Zip Code *" />
-          <Link to="/locations">FIND STORE</Link>
-        </div>
-      </div>
-      <Footer />
+  <div className="Hero">
+    <div className="HeroHeader">
+      <Carousel />
     </div>
+
+    <div className="OurFood">
+      <div className="OurFoodHeader">
+        <img style={{ transform: [{ rotateX: "180deg" }] }} src={accent}></img>
+        <h1>OUR FOOD</h1>
+        <img src={accent}></img>
+      </div>
+
+      <div className="OurFoodCategories">
+        <div className="DescriptionSection">
+          <img src={burrito}></img>
+          <h2>ENTREES</h2>
+        </div>
+        <div className="DescriptionSection">
+          <img src={tacos}></img>
+          <h2>MEALS</h2>
+        </div>
+        <div className="DescriptionSection">
+          <img src={white}></img>
+          <h2>SIDES</h2>
+        </div>
+      </div>
+
+      <Link to="/menu">VIEW MENU</Link>
+    </div>
+    <div className="Eclub">
+      <h1>JOIN THE ROAST</h1>
+      <div className="EclubForm">
+        <input type="text" name="email" value="email *" />
+        <Link to="/eclub">SIGN UP</Link>
+        <img src={brush}></img>
+      </div>
+    </div>
+
+    <div className="WhatsHappening">
+      <h1>WHAT'S HAPPENING</h1>
+      <div className="WhatsHappeningNews">
+        <News pic={doctors} />
+      </div>
+    </div>
+
+    <div className="Location">
+      <h1>FIND A LOCATION</h1>
+      <div className="EclubForm">
+        <input type="text" name="email" value="Zip Code *" />
+        <Link to="/locations">FIND STORE</Link>
+      </div>
+    </div>
+    <Footer />
   </div>
 );
 
