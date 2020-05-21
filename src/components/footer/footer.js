813c6c488d8b2891ede6db0e4d50dbb7c6c9e7f1
MrMushu <aaokura@gmail.com>
2020-05-20 23:38:28 -0700

    a

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,14 @@
-import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 import React from "react";
 import "./Footer.css";
-import facebook from "../../icons/facebook.png"
-import instagram from '../../icons/instagram.png'
-
-
-
+import facebook from "../../icons/facebook.png";
+import instagram from "../../icons/instagram.png";
 
 class Footer extends React.Component {
   constructor(props) {
@@ -19,12 +22,11 @@ class Footer extends React.Component {
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   };
   componentDidMount() {
-    window.addEventListener('resize', this.updateDimensions);
+    window.addEventListener("resize", this.updateDimensions);
     window.addEventListener("scroll", this.handleScroll);
   }
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions);
-
+    window.removeEventListener("resize", this.updateDimensions);
   }
 
   handleScroll = (event) => {
@@ -35,46 +37,50 @@ class Footer extends React.Component {
     } else {
       this.setState({ hasScrolled: false });
     }
-  }
+  };
 
   render() {
     return (
-      <div className="FooterContainer"> 
+      <div className="FooterContainer">
         <div className="About">
-             
-            <h3>About</h3>
-            <Link activeStyle={{
-                fontWeight: "bold",
-                color: "red"
-            }} to="/about-us">Our History</Link>
-            <Link to="/values">Our Values</Link>
+          <h3>Company</h3>
+          <Link
+            activeStyle={{
+              fontWeight: "bold",
+              color: "red",
+            }}
+            to="/about-us"
+          >
+            About Us
+          </Link>
+          <Link to="/values">Our Values</Link>
         </div>
 
-        <div >
-            <h3>Follow Us</h3>
-            <div className="Social">
-              <div className='Icon'> <img src={facebook}/></div>
-              <div className='Icon'> <img src={instagram}/></div>
+        <div>
+          <h3>Follow Us</h3>
+          <div className="Social">
+            <div className="Icon">
+              <img src={facebook} />
             </div>
-            
+            <div className="Icon">
+              <img src={instagram} />
+            </div>
+          </div>
         </div>
 
         <div className="Contact">
-      <h3>Contact Us</h3>
-            <h5>info@juanpollo.com</h5>
-            <h5>(909) 885-6324</h5>
+          <h3>Contact Us</h3>
+          <h5>info@juanpollo.com</h5>
+          <h5>(909) 885-6324</h5>
         </div>
 
         <div className="Disclaimer">
-            <h6>2020 Juan Pollo</h6>
-            <Link to="/terms">Terms of Use</Link>
-            <Link to="/privacy">Privacy Policy</Link>
-            <Link to="/accessibility">Accessibility</Link>
-
-
+          <h6>2020 Juan Pollo</h6>
+          <Link to="/terms">Terms of Use</Link>
+          <Link to="/privacy">Privacy Policy</Link>
+          <Link to="/accessibility">Accessibility</Link>
         </div>
       </div>
-
     );
   }
 }
