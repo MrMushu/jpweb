a916e54cecac429c6b5b9f909b9abbb261c1e846
MrMushu <aaokura@gmail.com>
2020-05-20 18:09:30 -0700

    a

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000;
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("src", path.join(__dirname, "src"))
-  .get("/", (req, res) => res.render("App.js"))
+  .get("/", (req, res) => res.render("index"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
