a31f589b61334fd444144b4705401acbabe07f2e
MrMushu <aaokura@gmail.com>
2020-05-21 00:32:53 -0700

    a

--- src/pages/catering.js
+++ src/pages/catering.js
@@ -49,7 +49,29 @@ class Catering extends React.Component {
             <Link>ORDER NOW</Link>
           </div>
         </div>
-        <div className="CateringPackages"></div>
+        <div className="CateringPackages">
+          <div className="PackageHeader">
+            <button>STANDARD</button>
+            <button>INDIVIDUALLY BOXED</button>
+            <button>CUSTOMIZE</button>
+          </div>
+          <div className="Package">
+            <div className="PackageDescription">
+              <h4>Starting $12 / Person</h4>
+              <p>
+                Our standard package comes with all the flavors you know and
+                love from Juan Pollo!
+              </p>
+              <p>
+                Chicken - Rice - Beans <br />
+                Potato Salad - Green Salad
+                <br />
+                Salsa - Tortillas
+              </p>
+            </div>
+            <img src={cateringHero}></img>
+          </div>
+        </div>
         <div className="HowItWorks">
           <h2>HOW IT WORKS</h2>
           <div className="HowSteps">
@@ -76,6 +98,50 @@ class Catering extends React.Component {
               <p>Sit back and enjoy the party!</p>
             </div>
           </div>
+        </div>
+        <div className="RequestForm">
+          <h2>REQUEST FORM</h2>
+          <p>
+            Fill in the form below to the best of your ability and we will reach
+            out to you about moving forward! You can also give us a call
+            directly to our corporate office at (999) 999-9999
+          </p>
+          <div className="RequestRow">
+            <input type="text" name="email" value="Name / Organization *" />
+            <input type="text" name="email" value="Phone Number *" />
+          </div>
+          <div className="RequestRow">
+            <input type="text" name="email" value="Email *" />
+            <input type="text" name="email" value="Catering Date *" />
+          </div>
+          <div className="RequestRow">
+            <input type="text" name="email" value="Event Location *" />
+            <input type="text" name="email" value="Event Type" />
+          </div>
+          <div className="RequestRow">
+            <input type="text" name="email" value="Approximate # of Guests" />
+            <input
+              type="text"
+              name="email"
+              value="Estimated Budget per Person"
+            />
+          </div>
+          <input
+            type="text"
+            name="email"
+            value="Additional Comments / Questions ..."
+          />
+
+          <Link>SUBMIT</Link>
+          <h6>
+            By clicking "Submit", I accept and agree to your Terms of Use and
+            acknowledge that I have received and reviewed your Privacy Policy.
+            <br />
+            Note: Fields with an asterisk (*) are required.
+          </h6>
+        </div>
+        <div className="CateringFAQ">
+          <h2>CATERING FAQ</h2>
         </div>
       </div>
     );
